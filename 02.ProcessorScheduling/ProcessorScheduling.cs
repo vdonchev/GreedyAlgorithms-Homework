7a4ef9388d1d520978d2bee696435f4534103748
vdonchev <ya.5164646777752015@yandex.ru>
2015-11-03 17:38:03 +0200

    upd

---
+++ 02.ProcessorScheduling/ProcessorScheduling.cs
@@ -0,0 +1,38 @@
+﻿namespace _02.ProcessorScheduling
+{
+    using System;
+    using System.Linq;
+
+    class ProcessorScheduling
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new[]
+            {
+                new {Index = 1, Value = 5, Deadtime = 3},
+                new {Index = 2, Value = 6, Deadtime = 4},
+                new {Index = 3, Value = 2, Deadtime = 1},
+                new {Index = 4, Value = 3, Deadtime = 4},
+                new {Index = 5, Value = 8, Deadtime = 2},
+                new {Index = 6, Value = 4, Deadtime = 3},
+            };
+
+            //var sortedTasks = tasks
+            //    .OrderBy(task => task.Deadtime)
+            //    .ThenByDescending(task => task.Value);
+
+            var sorted = tasks
+                .OrderByDescending(task => task.Value);
+
+            int turns = 1;
+            foreach (var task in tasks)
+            {
+                if (task.Deadtime >= turns)
+                {
+                    Console.Write(task.Index + " ");
+                    turns++;
+                }
+            }
+        } 
+    }
+}
