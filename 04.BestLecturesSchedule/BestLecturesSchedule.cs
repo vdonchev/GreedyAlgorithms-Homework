262d0ce28a138d5b404509eea3ff60474bfddb10
vdonchev <ya.5164646777752015@yandex.ru>
2015-11-04 09:45:04 +0200

    solved

---
+++ 04.BestLecturesSchedule/BestLecturesSchedule.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.BestLecturesSchedule
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class BestLecturesSchedule
+    {
+        static void Main(string[] args)
+        {
+            var lectures = new Dictionary<string, Tuple<int, int>>();
+            int count = int.Parse(Console.ReadLine().Split(' ')[1]);
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(new[] { ' ', '-', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                lectures.Add(tokens[0], new Tuple<int, int>(int.Parse(tokens[1]), int.Parse(tokens[2])));
+            }
+
+            var sortedLectures = lectures.OrderBy(l => l.Value.Item2);
+            int resCount = 0;
+            int earliestFinish = 0;
+            foreach (var lecture in sortedLectures)
+            {
+                if (lecture.Value.Item1 >= earliestFinish)
+                {
+                    resCount++;
+                    earliestFinish = lecture.Value.Item2;
+                }
+                else
+                {
+                    lectures.Remove(lecture.Key);
+                }
+            }
+
+            Console.WriteLine($"Lectures ({resCount}):");
+            foreach (var lecture in lectures.OrderBy(l => l.Value.Item2))
+            {
+                Console.WriteLine($"{lecture.Value.Item1}-{lecture.Value.Item2} -> {lecture.Key}");
+            }
+        }
+    }
+}
